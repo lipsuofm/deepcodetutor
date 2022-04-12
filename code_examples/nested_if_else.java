42eea8996025243849b27b7a49835305972d1429
Rabin Banjade <rabin.banjade@gmail.com>
2022-04-12 15:18:16 -0500

    updated nested if

--- code_examples/nested_if_else.java
+++ code_examples/nested_if_else.java
@@ -3,15 +3,18 @@ public class EvenOdd {
 
 		int num = 8;
 
-		if (num % 2 == 0) {
-			if (num<10) {
+		if (num < 10) {
+			if (num%2==0) {
 				System.out.println(num + " is even number less than 10");
 			} else {
-				System.out.println(num + " is even number greater than 10");
+				System.out.println(num + " is odd number less than 10");
 			}
-		} else {
-			System.out.println(num + " is odd");
+			
 		}
+		else{
+		    System.out.println(num + " is greater than 10");
+		}
+		
 
 	}
 }
